f041a282781fa9639e1f076b83312874c89b2747
Ignacio <1992ignacio@gmail.com>
2024-04-15 00:51:05 -0700

    Initial Commit

---
+++ Lululemon/LululemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LululemonApp.swift
+//  Lululemon
+//
+//  Created by Ignacio Arias on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct LululemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
